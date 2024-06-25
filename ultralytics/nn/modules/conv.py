593d484db2d5c0078b2f668930a4b1d2638367af
ABHISHEK RAJ <68436537+abhishekraj2706@users.noreply.github.com>
2024-06-25 18:57:47 +0530

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -17,6 +17,7 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
         k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+        print("auto")
     return p
 
 
